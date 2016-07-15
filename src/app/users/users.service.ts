590001a931aa00b13716fb498ab7fc647702efcd
Jerome <jerome@gaatech.be>
2016-07-15 15:35:19 +0200

    some changes

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,19 +1,27 @@
-import {Injectable} from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
 import {jsonrpcService} from '../jsonrpc/jsonrpc.service';
 import {User} from '../user/user';
 
 @Injectable()
 export class UsersService  {
     public list: Array<User>;
+    public connectedCount: ConnectedUsersCount;
 
     constructor(
         private _rpc: jsonrpcService
     ) {
         this._rpc.Register("UsersService.updateList", this.updateList.bind(this));
         this._rpc.Register("UsersService.removeFromList", this.removeFromList.bind(this));
+        this._rpc.Register("UsersService.setConnectedCount", this.setConnectedCount.bind(this));
+    }
+
+    ngOnInit() {
         this._rpc.PromiseCall("UserRPCService.GetAll", "")
             .then(users => this.list = users)
             .catch(error => console.log(error));
+        this._rpc.PromiseCall("UserRPCService.GetConnectedCount", "")
+            .then(result => this.setConnectedCount(result))
+            .catch(err => console.log(err))
     }
 
     updateList(user: User, oldUsername: String = null): boolean {
@@ -42,4 +50,18 @@ export class UsersService  {
         this.list = tmp;
         return true
     }
+
+    setConnectedCount(data: ConnectedUsersCount) {
+        this.connectedCount = data;
+    }
+
+    sumConnected(): number {
+        return this.connectedCount.anonymous + this.connectedCount.notRegistered + this.connectedCount.registered;
+    }
+}
+
+export interface ConnectedUsersCount {
+    anonymous: number;
+    notRegistered: number;
+    registered: number;
 }
